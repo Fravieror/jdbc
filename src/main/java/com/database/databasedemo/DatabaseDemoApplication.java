b6ec8850d4e8683a9e07a79a788721eb6e419ea4
FJ <lfjl@live.com>
2021-08-24 16:55:35 -0500

    Add Insert and update

--- src/main/java/com/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/database/databasedemo/DatabaseDemoApplication.java
@@ -1,5 +1,6 @@
 package com.database.databasedemo;
 
+import com.database.databasedemo.entity.Person;
 import com.database.databasedemo.jdbc.PersonDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
 
 @SpringBootApplication
 public class DatabaseDemoApplication implements CommandLineRunner {
@@ -24,6 +27,8 @@ public class DatabaseDemoApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         logger.info("All persons -> {}", dao.findAll().toString());
         logger.info("User id 10001 -> {}", dao.findByID(10001));
-        logger.info("Deleting User id 10002 -> No rows Deleted {}", dao.deleteByID(10002));
+        logger.info("Deleting User id 10002 -> No rows deleted {}", dao.deleteByID(10002));
+        logger.info("Inserting User id 10007 -> No rows inserted {}", dao.insert(new Person(10007, "Tara", "Berlin", new Date())));
+        logger.info("Updating User id 10004 -> No rows updated {}", dao.update(new Person(10004, "Pierce", "Amsterdan", new Date())));
     }
 }
